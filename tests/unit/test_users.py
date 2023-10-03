88aa659c07fc806ea80a58aeaed0a8046115cb5d
dildeolupbiten <tckutlu@gmail.com>
2023-10-03 11:46:21 +0200

    Update

--- tests/unit/test_users.py
+++ tests/unit/test_users.py
@@ -2,7 +2,7 @@
 
 import os
 from dildeolupbiten.users.models import User
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user
 
 
 def test_register(app, client, test_user):
@@ -37,7 +37,7 @@ def test_login(client, test_user):
     # Send a post request with wrong credentials.
     response = client.post("/login", data=data, headers={"X-Requested-With": "XMLHttpRequest"})
     assert response.status_code == 404
-    assert "Redirecting" in response.data.decode()
+    assert b"Redirecting" in response.data
     # Use the password of the test user.
     data["password"] = "test1234"
     # Send a post request with test user credentials.
@@ -52,7 +52,7 @@ def test_login(client, test_user):
     assert b"Redirecting" in response.data
 
 
-def test_views(client):
+def test_view_user_articles(client):
     for i in ["", "/all_articles"]:
         # Send a post request for a view that doesn't exist.
         response = client.get("/user/invalid_username" + i)
@@ -65,9 +65,90 @@ def test_views(client):
         assert response.status_code == 200
 
 
-def test_delete(app, client):
+def test_update_user(app, client):
     with app.test_request_context():
-        # Send a post request without login.
+        response = client.get("/account")
+        assert response.status_code == 302
+        assert b"Redirecting" in response.data
+        data = {
+            "username": "dildeolupbiten",
+            "email": "test@test.com"
+        }
+        # Send a post request without credentials
+        response = client.post("/account", data=data, headers={"X-Requested-With": "XMLHttpRequest"})
+        assert response.status_code == 302
+        assert b"Redirecting" in response.data
+        user = User.query.filter_by(username="dildeolupbiten").first()
+        # Assert that the email hasn't changed.
+        assert user.email != "test@test.com"
+        # Now let's change the user. Set the transaction user_id.
+        with client.session_transaction() as session_transaction:
+            session_transaction['user_id'] = user.id
+        # Login as test_user.
+        login_user(user)
+        # Send a post request without credentials
+        response = client.post("/account", data=data, headers={"X-Requested-With": "XMLHttpRequest"})
+        assert response.status_code == 302
+        assert b"Redirecting" in response.data
+        # Assert that the email has changed.
+        assert User.query.filter_by(username="dildeolupbiten").first().email == "test@test.com"
+        # Change the email back to original
+        data["email"] = "dildeolupbiten@gmail.com"
+        # Send a post request without credentials
+        response = client.post("/account", data=data, headers={"X-Requested-With": "XMLHttpRequest"})
+        assert response.status_code == 302
+        assert b"Redirecting" in response.data
+        # Assert that the email has changed to original.
+        assert User.query.filter_by(username="dildeolupbiten").first().email == "dildeolupbiten@gmail.com"
+
+
+def test_reset_request(client):
+    # Send a get request.
+    response = client.get("/reset_password")
+    assert response.status_code == 200
+    data = {"email": "dildeolupbiten@gmail.com"}
+    # Send a post request, this request sends an email.
+    response = client.post("/reset_password", data=data, headers={"X-Requested-With": "XMLHttpRequest"})
+    assert response.status_code == 302
+    assert b"Redirecting" in response.data
+
+
+def test_reset_password(app, client):
+    with app.test_request_context():
+        # Send a get request.
+        response = client.get("/reset_password")
+        assert response.status_code == 200
+        # Get the user
+        user = User.query.filter_by(username="dildeolupbiten").first()
+        # We will use get_reset_token() function to create a new token.
+        token = user.get_reset_token()
+        # Verify the token
+        user = User.verify_reset_token(token)
+        assert user
+        data = {"password": "new1234", "confirm_password": "new1234"}
+        response = client.post(f"/reset_password/{token}", data=data, headers={"X-Requested-With": "XMLHttpRequest"})
+        assert response.status_code == 302
+        assert b"Redirecting" in response.data
+        # Let's try to log in with the old password.
+        data = {
+            "email": "dildeolupbiten@gmail.com",
+            "password": "dildeolupbiten1234"
+        }
+        # Send a post request with the old password.
+        response = client.post("/login", data=data, headers={"X-Requested-With": "XMLHttpRequest"})
+        assert response.status_code == 404
+        assert b"Redirecting" in response.data
+        # Change the password.
+        data["password"] = "new1234"
+        # Send a post request with the new password.
+        response = client.post("/login", data=data, headers={"X-Requested-With": "XMLHttpRequest"})
+        assert response.status_code == 302
+        assert b"Redirecting" in response.data
+
+
+def test_delete_user(app, client):
+    with app.test_request_context():
+        # Send a get request.
         response = client.get("/user/dildeolupbiten/delete")
         assert response.status_code == 302
         # Let's check whether the user exists.
@@ -83,3 +164,20 @@ def test_delete(app, client):
         assert response.status_code == 302
         # Assert user is deleted.
         assert User.query.filter_by(username="dildeolupbiten").first() is None
+
+
+def test_logout(client, test_user):
+    # Send a get request.
+    response = client.get("/logout")
+    assert response.status_code == 302
+    assert b"Redirecting" in response.data
+    assert hasattr(current_user, "username") is False
+    with client.session_transaction() as session_transaction:
+        session_transaction['user_id'] = test_user.id
+    # Login as test_user.
+    login_user(test_user)
+    assert hasattr(current_user, "username")
+    # Now log out as.
+    logout_user()
+    # Let's assert that test_user successfuly logged out.
+    assert hasattr(current_user, "username") is False
