46359f5aa075c9e998af6be14437f414bdac441c
dildeolupbiten <tckutlu@gmail.com>
2023-10-17 15:35:35 +0200

    Update

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def test_get_requests(client):
-    for view in ["/", "/about", "/all_articles"]:
+    for view in ["/", "/all_articles"]:
         response = client.get(view)
         assert response.status_code == 200
 
